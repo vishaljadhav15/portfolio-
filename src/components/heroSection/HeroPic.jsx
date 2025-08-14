e38d2ef2b27c5d5763d009dd7f2b9e49e68739c7
vishaljadhav15 <vishaljadhav.11022002@gmail.com>
2025-08-14 13:12:35 +0530

    Initial commit

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -12,7 +12,7 @@ const HeroPic = () => {
       className=" h-full flex items-center justify-center "
     >
       <img
-        src="public/images/vishal.png"
+        src="/images/vishal.png"
         alt="Vishal Jadhav"
         className="max-h-[450px] w-auto"
       />
--- src/components/skillsSection/SubSkills.jsx
+++ src/components/skillsSection/SubSkills.jsx
@@ -2,11 +2,7 @@ const SubSkills = () => {
   return (
     <div className="border-y-2 border-lightGrey relative ">
       <div className="absolute bg-gradient-to-r from-orange to-[#b27f26] opacity-50 w-full h-full"></div>
-      <img
-        src="../../public/images/subSkills.jpg"
-        alt="subSkills image"
-        className=""
-      />
+      <img src="/images/subSkills.jpg" alt="subSkills image" className="" />
     </div>
   );
 };
