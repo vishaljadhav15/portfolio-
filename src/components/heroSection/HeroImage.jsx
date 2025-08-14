44759a8d411884c414844df3bb4d3eec346a6de8
vishaljadhav15 <vishaljadhav.11022002@gmail.com>
2025-08-14 12:59:00 +0530

    Initial commit

--- src/components/heroSection/HeroImage.jsx
+++ src/components/heroSection/HeroImage.jsx
@@ -5,7 +5,7 @@ const HeroImage = () => {
     <div className="relative self-end h-full w-full items-center justify-center">
       <div className=" h-full w-full">
         <img
-          src="/images/vishal.png"
+          src="public/images/vishal.png"
           alt="Vishal Jadhav"
           className="w-auto h-auto md:max-w-[570px] sm:max-w-[380px] absolute bottom-[0px] z-10 left-[50%] -translate-x-[50%]"
         />
